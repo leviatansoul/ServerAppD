08f72ee4fcc32bac3a6dad3c8645b55113a65bfa
Diego <diego2.gd@gmail.com>
2019-12-31 12:50:15 +0100

    Agregar archivos de proyecto.

---
+++ ServerAppDynamics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerAppDynamics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am a Server!");
+        }
+    }
+}
