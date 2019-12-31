f1f284fd083b3311bf6b9853bd817c932d6e0e35
Diego <diego2.gd@gmail.com>
2019-12-31 12:56:26 +0100

    TCP/IP connection stablished

--- ServerAppDynamics/Program.cs
+++ ServerAppDynamics/Program.cs
@@ -1,12 +1,115 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+// A C# Program for Server 
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerAppDynamics
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("I am a Server!!!");
-        }
-    }
+	class Program
+	{
+
+		// Main Method 
+		static void Main(string[] args)
+		{
+			ExecuteServer();
+		}
+
+
+		public static void ExecuteServer()
+		{
+			// Establish the local endpoint 
+			// for the socket. Dns.GetHostName 
+			// returns the name of the host 
+			// running the application. 
+			IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+			// Creation TCP/IP Socket using 
+			// Socket Class Costructor 
+			Socket listener = new Socket(ipAddr.AddressFamily,
+						SocketType.Stream, ProtocolType.Tcp);
+
+			try
+			{
+
+				// Using Bind() method we associate a 
+				// network address to the Server Socket 
+				// All client that will connect to this 
+				// Server Socket must know this network 
+				// Address 
+				listener.Bind(localEndPoint);
+
+				// Using Listen() method we create 
+				// the Client list that will want 
+				// to connect to Server 
+				listener.Listen(10);
+
+				while (true)
+				{
+
+					Console.WriteLine("Waiting connection ... ");
+
+					// Suspend while waiting for 
+					// incoming connection Using 
+					// Accept() method the server 
+					// will accept connection of client 
+					Socket clientSocket = listener.Accept();
+
+					// Data buffer 
+					byte[] bytes = new Byte[1024];
+					string data = null;
+					string[] args = null;
+					byte[] message = null;
+
+					while (true)
+					{
+
+						int numByte = clientSocket.Receive(bytes);
+
+						data += Encoding.ASCII.GetString(bytes,
+												0, numByte);
+
+						if (data.IndexOf("<EOF>") > -1)
+						{
+							data = data.Substring(0, data.Length - 5);
+							args = data.Split(' ');
+							break;
+						}
+
+					}
+
+					Console.WriteLine("Text received -> {0} ", data);
+					Console.WriteLine("Args: " + string.Join(",", args));
+
+					message = Encoding.ASCII.GetBytes(data);
+
+
+
+					// Send a message to Client 
+					// using Send() method 
+					clientSocket.Send(message);
+
+					// Close client Socket using the 
+					// Close() method. After closing, 
+					// we can use the closed Socket 
+					// for a new Client Connection 
+					clientSocket.Shutdown(SocketShutdown.Both);
+					clientSocket.Close();
+				}
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine(e.ToString());
+			}
+		}
+	}
 }
